5e8fb1deee33549bf4be7624c93931bdc46e961c
belkrj <belkrj@rose-hulman.edu>
2018-02-05 00:16:01 -0500

    All TODO'S DONE.

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -2,8 +2,8 @@
 This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Robert Belk.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -12,37 +12,58 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI with stuff on it. """
     # ------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DONE: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # ------------------------------------------------------------------
+    root = tkinter.Tk()
+    root.title('Saying hello or goodbye!')
 
     # ------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DONE: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # ------------------------------------------------------------------
-
+    frame1 = ttk.Frame(root, padding=10)
+    frame1.grid()
     # ------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DONE: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # ------------------------------------------------------------------
-
+    btn = ttk.Button(frame1, text='Say Hello')
+    btn.grid()
     # ------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # DONE: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # ------------------------------------------------------------------
+    btn['command'] = lambda: print('Hello!')
 
     # ------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DONE: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # ------------------------------------------------------------------
+    entry = ttk.Entry(frame1)
+    entry.grid()
+
+    btn2 = ttk.Button(frame1, text='Say Hello or Goodbye')
+    btn2.grid()
+
+    btn2['command'] = lambda: print_hello_or_goodbye(entry)
+
+    entry2 = ttk.Entry(frame1)
+    entry2.grid()
+
+    btn3 = ttk.Button(frame1, text='Print your string')
+    btn3.grid()
+
+    btn3['command'] = lambda: print_string(entry, entry2)
+
 
     # ------------------------------------------------------------------
-    # TODO: 7.
+    # DONE: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -66,9 +87,47 @@ def main():
     ####################################################################
 
     # ------------------------------------------------------------------
-    # TODO: 8. As time permits, do other interesting GUI things!
+    # DONE: 8. As time permits, do other interesting GUI things!
     # ------------------------------------------------------------------
+    root2 = tkinter.Tk()
+    root2.title('Answering Questions.')
 
+    frame2 = ttk.Frame(root2, padding=20, )
+    frame2.grid()
+
+    inquiry = ttk.Entry(frame2)
+    inquiry.grid()
+
+    btn21 = ttk.Button(frame2, text='Hey Stranger!')
+    btn21['command'] = lambda : responding(inquiry)
+    btn21.grid()
+
+
+
+    root.mainloop()
+    root2.mainloop()
+
+
+def print_hello_or_goodbye(entry):
+    contents = entry.get()
+    if contents == 'ok':
+        print('Hello!')
+    else:
+        print('Goodbye!')
+
+
+def print_string(string, n):
+    number_of_times = int(n.get())
+    contents = string.get()
+    for k in range(number_of_times):
+        print(contents)
+
+def responding(saying):
+    contents = saying.get()
+    if contents == 'What is up?':
+        print('The sky.')
+    if contents == 'How are you?':
+        print('I am fantastic!')
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
